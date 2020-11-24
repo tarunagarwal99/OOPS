c0fc316ca5ab16395484e673f5891a5775244776
Tarun Prahladka <tarunprahladka965@gmail.com>
2020-11-24 22:44:17 +0530

    .

---
+++ practise25.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int x=-1;
+    unsigned int y=2;
+    if(x>y){
+        cout<<"x is greater";
+    }
+    else{
+        cout<<"y is greater";
+    }
+    return 0;
+}
